947690924b369587ce9117e8f0bbeff1e15e6bc6
Bolous AbuJaber <ajbolous@gmail.com>
2016-03-21 18:32:18 +0200

    django addition

--- handServer/controller/views.py
+++ handServer/controller/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from FirstHand.python.hand import Hand
 # Create your views here.
+
+def move(request):
+    h = Hand()
+    print(h)
+    return HttpResponse('you requested me ')
