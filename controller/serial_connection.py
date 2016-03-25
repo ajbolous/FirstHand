94b5ee0d732b7f01fa5acb9daa87c1b05fe10ea6
Ahdab Serhan <ms.ahdab@gmail.com>
2016-03-25 11:06:14 +0300

    added a new function- create and add packet

--- controller/serial_connection.py
+++ controller/serial_connection.py
@@ -15,7 +15,10 @@ class SerialConnection(object):
         self._serial.write(data)
 
     def create_packet(self,angles):
-        data = [0,0,angles[0],angles[1],angles[2],angles[3],0,0]
+        data = [angles[0],angles[1],angles[2],angles[3],0,0]
         data=bytearray(data)
         return str(data)
 
+    def create_send_package(self,angles):
+        packet=self.create_packet(angles)
+        self.send(packet)
