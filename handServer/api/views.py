de31793c149fec7c6faf4bd09e149d3545780071
Bolous AbuJaber <ajbolous@gmail.com>
2016-03-29 22:55:18 +0300

    added a small gui

--- handServer/api/views.py
+++ handServer/api/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 
-#from handControl.hand import *
+from handControl.hand import *
+import random
 
 
 # Create your views here.
@@ -10,5 +11,5 @@ def move(request):
    y=int(request.GET['y'])
    z=int(request.GET['z'])
    w=int(request.GET['w'])
- 
+   hand.move([x,y,z,w])
    return HttpResponse('you requested : {}, {}, {}, {}'.format(x,y,z,w))
